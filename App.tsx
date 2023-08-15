8de41684eedcbc01b3b1e0bb29e1c2a692662381
Pasha <pashaie@gmail.com>
2023-08-15 21:28:45 +0330

    spring!

--- App.tsx
+++ App.tsx
@@ -42,6 +42,7 @@ export default function App() {
 
   const onReset = () => {
     setShowAppOptions(false);
+    setPickedEmoji(null);
   };
 
   return (
--- components/EmojiSticker.tsx
+++ components/EmojiSticker.tsx
@@ -1,13 +1,41 @@
 import { View, Image } from "react-native";
+import Animated, {
+  useAnimatedStyle,
+  useSharedValue,
+  useAnimatedGestureHandler,
+  withSpring,
+} from "react-native-reanimated";
+import { TapGestureHandler } from "react-native-gesture-handler";
+
+const AnimatedImage = Animated.createAnimatedComponent(Image);
 
 export default function EmojiSticker({ imageSize, stickerSource }: any) {
+  const scaleImage = useSharedValue(imageSize);
+
+  const onDoubleTap = useAnimatedGestureHandler({
+    onActive: () => {
+      if (scaleImage.value !== imageSize * 2) {
+        scaleImage.value = scaleImage.value * 2;
+      }
+    },
+  });
+
+  const imageStyle = useAnimatedStyle(() => {
+    return {
+      width: withSpring(scaleImage.value),
+      height: withSpring(scaleImage.value),
+    };
+  });
+
   return (
     <View style={{ top: -300, left: 20 }}>
-      <Image
-        source={stickerSource}
-        resizeMode="contain"
-        style={{ width: imageSize, height: imageSize }}
-      />
+      <TapGestureHandler numberOfTaps={2} onGestureEvent={onDoubleTap}>
+        <AnimatedImage
+          source={stickerSource}
+          resizeMode="contain"
+          style={[imageStyle, { width: imageSize, height: imageSize }]}
+        />
+      </TapGestureHandler>
     </View>
   );
 }
